e1181f6f8f416bebbcc5d2c94dd4185536a9bdb3
David Rakosi <david.rakosi99@gmail.com>
2020-07-31 02:33:51 +0200

    Revert "Revert "Revert "CHNG: design"""
    This reverts commit e1af827a46b2ad12a5baf5b8c55c9e704b1b71f7.

--- script/master.js
+++ script/master.js
@@ -22,18 +22,18 @@
 
 setUpNavButtons = (data) => {
     // infected - active
-    document.getElementById('active-delta').innerText = `Today: ${addDecimals(data.todayCases)}`
+    document.getElementById('active-delta').innerText = addDecimals(data.todayCases)
     document.getElementById('active-total').innerText = numbersFriendlyFormat(data.active)
 
     // deaths
-    document.getElementById('deaths-delta').innerText = `Today: ${addDecimals(data.todayDeaths)}`
+    document.getElementById('deaths-delta').innerText = addDecimals(data.todayDeaths)
     document.getElementById('deaths-total').innerText = numbersFriendlyFormat(data.deaths)
 
     // recovered
-    document.getElementById('recovered-delta').innerText = `Today: ${addDecimals(data.todayRecovered)}`
+    document.getElementById('recovered-delta').innerText = addDecimals(data.todayRecovered)
     document.getElementById('recovered-total').innerText = numbersFriendlyFormat(data.recovered)
 
     // total cases
-    document.getElementById('cases-delta').innerText = `Today: ${addDecimals(data.cases)}`
+    document.getElementById('cases-delta').innerText = addDecimals(data.cases)
     document.getElementById('cases-total').innerText = numbersFriendlyFormat(data.cases)
 }
